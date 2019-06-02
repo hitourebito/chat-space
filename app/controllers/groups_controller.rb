882fa1c958d7c1682ba44af643d6a8f6ff205458
hitourebito <yuto0423777@gmail.com>
2019-06-02 14:21:04 +0900

    define action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,22 @@
 class GroupsController < ApplicationController
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def method_name
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
 end
