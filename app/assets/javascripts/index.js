8ed552fec4b5e85eca9577a22ea0e979ca5e26bb
hitourebito <yuto0423777@gmail.com>
2019-06-28 00:50:56 +0900

    add index.js code and rename index.json.jbuilder

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,45 @@
 $(function() {
-  $("#user-search-field").on("keyup", function() {
+
+  function appendUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>`
+    $('#user-search-result').append(html);
+  }
+
+  function appendErrMsgToHTML(msg){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${msg}</p>
+                </div>`
+    $('#user-search-result').append(html);
+  }
+
+  $("#user-search-field").on("keyup", function(e) {
+    e.preventDefault();
     var input = $("#user-search-field").val();
     console.log(input);
+    
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+    
+    .done(function(users){
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendErrMsgToHTML("ユーザー検索に失敗しました");
+      }
+    })
+    .fail(function(){
+      alert("ユーザー検索に失敗しました");
+    })
   });
 });
--- app/views/users/index.json.jbuilder
+++ app/views/users/index.json.jbuilder
@@ -1,2 +1,3 @@
 json.array! @users do |user|
+  json.id user.id
   json.name user.name
