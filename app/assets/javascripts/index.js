407b85f843173e04d9ba0fee82a0826de8dfc007
hitourebito <yuto0423777@gmail.com>
2019-06-28 15:28:08 +0900

    add chatmembers function

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -5,7 +5,6 @@ $(function() {
                   <p class="chat-group-user__name">${user.name}</p>
                   <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
                 </div>`
-    $('#user-search-result').empty();
     $('#user-search-result').append(html);
   }
 
@@ -15,6 +14,15 @@ $(function() {
                 </div>`
     $('#user-search-result').empty();
     $('#user-search-result').append(html);
+  }
+
+  function appendChatMembers(name, user_id) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                  <p class='chat-group-user__name'>${name}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>`
+    $("#add-chat-members").append(html);
   }
 
   $("#user-search-field").on("keyup", function(e) {
@@ -30,6 +38,7 @@ $(function() {
       dataType: 'json'
     })
     .done(function(users){
+      $('#user-search-result').empty();
       if (users.length !== 0) {
         users.forEach(function(user){
           appendUser(user);
@@ -43,9 +52,19 @@ $(function() {
       alert("ユーザー検索に失敗しました");
     })
   });
+
+$(function(){
   $("#user-search-result").on("click", ".user-search-add.chat-group-user__btn.chat-group-user__btn--add", function(e){
-    e.preventDefault;
     //確認用console.log
-    console.log("OK");
+    console.log(this);
+    var name = $(this).data("user-name");
+    var user_id = $(this).data("user-id");
+    $(this).parent().remove();
+    appendChatMembers(name, user_id);
   });
+
+  $("#add-chat-members").on("click", ".chat-group-user.clearfix.js-chat-member", function(){
+    $(this).parent().remove();
+  })
+ });
 });
